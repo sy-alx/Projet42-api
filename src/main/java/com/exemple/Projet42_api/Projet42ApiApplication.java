28a307e88967bdd765fff58b9a77e44c38670ca4
polak <SyrillAlixSySy@outlook.com>
2024-06-05 14:49:31 +0200

    Initial commit

---
+++ src/main/java/com/exemple/Projet42_api/Projet42ApiApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.Projet42_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet42ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet42ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemple/Projet42_api/Projet42ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.Projet42_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet42ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
